7d7d286e43cb800ac974c177ee72086770eb30a1
Sadid <sfhsadid@gmail.com>
2023-03-26 09:13:20 +0600

    appointmentserves area done

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fdmrdur.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const appointmentServesCollection = client
+      .db("healthServes")
+      .collection("appointmentserves");
+    app.get("/appointmentserves", async (req, res) => {
+      const query = {};
+      const allOptions = await appointmentServesCollection
+        .find(query)
+        .toArray();
+      res.send(allOptions);
+    });
+  } finally {
+  }
+}
+run().catch(console.log("error"));
 
 app.get("/", async (req, res) => {
   res.send("Health Serves Is Running");
