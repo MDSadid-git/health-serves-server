602f886c4155146b3de151b53172471f2f3efe48
Sadid <sfhsadid@gmail.com>
2023-03-26 10:02:55 +0600

    Git area Ignore done

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fdmrdur.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
